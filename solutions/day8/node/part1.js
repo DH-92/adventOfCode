f156a207f15f459a0b388f6b24db0d2b0bd81e4a
Douglas Hodson <douglas@r6digital.com.au>
2021-12-08 21:51:54 +1000

    adding solution for day8 parts 1 and 2

---
+++ solutions/day8/node/part1.js
@@ -0,0 +1,27 @@
+const fs = require('fs')
+
+const INITIAL_STATE_FILE = "day8.txt" //question
+// const INITIAL_STATE_FILE = "day8e.txt" //example
+
+const wordsByLength = new Array(10).fill(0)
+
+const initialState =
+    fs.readFileSync(INITIAL_STATE_FILE)
+        .toString()
+        .split('\n')
+        .filter((x) => { return x.length != 0 })
+        .map((line) => {
+            const splitPipe = line.split('|')
+            // const inputWords =
+            //     splitPipe[0].split(' ')
+            //         .filter((x) => { return x.length != 0 })
+            const outputWords =
+                splitPipe[1].split(' ')
+                    .filter((x) => { return x.length != 0 })
+            for (word of outputWords) {
+                wordsByLength[word.length]++
+            }
+        })
+const sum = wordsByLength[2] + wordsByLength[3] + wordsByLength[4] + wordsByLength[7]
+console.log(sum)
+process.exit()
