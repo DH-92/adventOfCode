d0f7d6b40378be53ead4347dcd34eb575beca662
Douglas Hodson <douglas@r6digital.com.au>
2022-12-06 16:07:20 +1000

    22/day6 solution

--- 2022/js/day6/solution.mjs
+++ 2022/js/day6/solution.mjs
@@ -1,4 +1,30 @@
 #!/usr/bin/env zx
-const input="../../input/day6/input.txt"
-const example="../../input/day6/example.txt"
+import "zx/globals"
+import { fileToArr } from "../helpers/index.mjs"
+const day = "day6";
+const input = `../../input/${day}/input.txt`
+const example = `../../input/${day}/example.txt`
 
+function isDistinct(array) {
+    return (new Set(array)).size === array.length;
+}
+
+const parseInput = (path, target) => {
+    const input = fileToArr(path, '')
+    const stack = [];
+    let pos = 0;
+    while (input.length !== 0) {
+        pos++;
+        stack.push(input.shift())
+        if (stack.length === target) {
+            if (isDistinct(stack)) return pos;
+            stack.shift()
+        }
+    }
+}
+
+echo(`part1 -- example: ${parseInput(example,4)}`)
+echo(`part1 -- input: ${parseInput(input,4)}`)
+
+echo(`part2 -- example: ${parseInput(example,14)}`)
+echo(`part2 -- input: ${parseInput(input,14)}`)
