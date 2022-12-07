11b5b20b6584d968c2f2ac5f079d3973a2152512
Douglas Hodson <douglas@r6digital.com.au>
2022-12-07 18:46:37 +1000

    22/day7 readability pass

--- 2022/js/day7/solution.mjs
+++ 2022/js/day7/solution.mjs
@@ -1,6 +1,6 @@
 #!/usr/bin/env zx
 import "zx/globals"
-import { fileToArr,sum, numSort } from "../helpers/index.mjs"
+import { fileToArr, sum, numSort } from "../helpers/index.mjs"
 const day = "day7";
 const input = `../../input/${day}/input.txt`
 const example = `../../input/${day}/example.txt`
@@ -9,59 +9,70 @@ const TargetDirSize = 100000;
 const diskSize = 70000000;
 const requiredFree = 30000000;
 
-const parseInput = (path) => {
-    const dirs = [];
-    class Dir {
-        children = {};
-        size = 0;
-        constructor(name, parent) {
-            this.name = name;
-            this.parent = parent;
-        }
-        du = () => {
-            Object.values(this.children).forEach(child => this.size += child.du())
-            dirs.push(this.size);
-            return this.size;
-        }
-
+class Dir {
+    children = {};
+    size = 0;
+    constructor(name, parent) {
+        this.name = name;
+        this.parent = parent;
+        this.dirs = parent ? parent.dirs : [];
+        this.root = parent ? parent.root : this;
     }
+    du = () => {
+        Object.values(this.children).forEach(child => this.size += child.du())
+        this.dirs.push(this.size);
+        return this.size;
+    }
+}
 
+const parseInput = (path) => {
     const root = new Dir("/", null);
-    let pwd = root;
+    let cwd = root;
 
     fileToArr(path).forEach(line => {
         const args = line.split(' ');
-        switch(args[0]) {
+        switch (args[0]) {
             case "$":
+                // commands[args[1]](cwd,args[2]); break
                 switch (args[1]) {
                     case "cd":
                         const target = args[2];
-                        pwd = (target === "/")
-                            ? root
+                        cwd = (target === "/")
+                            ? cwd.root
                             : (target === "..")
-                                ? pwd.parent
-                                : pwd.children[args[2]];
+                                ? cwd.parent
+                                : cwd.children[target];
                         break;
                     case "ls":
                     case "default":
                 }
                 break;
+            //as ls is the only command with output assume we're in ls's output
             case "dir":
-                pwd.children[args[1]] = new Dir(args[0], pwd);
-                break;
+                cwd.children[args[1]] = new Dir(args[0], cwd); break;
             default: //it's a folder
-                pwd.size += +args[0];
+                cwd.size += +args[0];
         }
     })
-    
-    const sizeToFree = requiredFree - (diskSize - root.du(root));
-    const smallestDeletable = dirs.filter(size => size >= sizeToFree).sort(numSort)[0];
-    const dirsUnderLimit = dirs.filter(size => size <= TargetDirSize).reduce(sum);
-    return [dirsUnderLimit, smallestDeletable];
+    root.du();
+    return root;
+}
+const part1 = (path) => {
+    return parseInput(path).dirs
+        .filter(size => size <= TargetDirSize)
+        .reduce(sum);
 }
 
-echo(`part1 -- example: ${parseInput(example)[0]}`)
-echo(`part1 -- input: ${parseInput(input)[0]}`)
+const part2 = (path) => {
+    const root = parseInput(path);
+    const sizeToFree = requiredFree - (diskSize - root.size);
+    return root.dirs
+        .filter(size => size >= sizeToFree)
+        .sort(numSort)[0];
+}
 
-echo(`part2 -- example: ${parseInput(example)[1]}`)
-echo(`part2 -- input: ${parseInput(input)[1]}`)
+echo(`part1 -- example: ${part1(example)}`)
+echo(`part1 -- input: ${part1(input)}`)
+
+echo(`part2 -- example: ${part2(example)}`)
+echo(`part2 -- input: ${part2(input)}`)
