eb341251c3e3bdbd7fbf0e12d9889747f543151d
Douglas Hodson <douglas@r6digital.com.au>
2022-12-07 16:52:58 +1000

    add solution for 22/day7

--- 2022/js/day5/solution.mjs
+++ 2022/js/day5/solution.mjs
@@ -1,10 +1,6 @@
 #!/usr/bin/env zx
 import "zx/globals"
-import {
-    fileToArr,
-    sum,
-    numSort,
-} from "../helpers/index.mjs"
+import { fileToArr } from "../helpers/index.mjs"
 const day = "day5";
 const input = `../../input/${day}/input.txt`
 const example = `../../input/${day}/example.txt`
@@ -15,38 +11,50 @@ const LINE = '\n';
 
 const countStacks = (arr) => +arr.pop().split(WORD).filter(x => !!x).pop()
 
-const stackToCol = (i) => { return (4 * (i - 1)) + 1 }
+const stackToCol = (i) => (4 * (i - 1)) + 1
 
 const buildStacks = (stacks, line) => {
     stacks.forEach((stack, index) => {
-        if (line[stackToCol(index)] &&
-            line[stackToCol(index)].trim()
-        ) stack.push(line[stackToCol(index)]);
+        if (line[stackToCol(index)]?.trim()) {
+            stack.push(line[stackToCol(index)])
+        }
     })
     return stacks;
 }
 
+const parseRules = (rule) => {
+    //move ${count} from ${from} to ${to}
+    const [, count, , from, , to] = rule.split(WORD);
+    return { count, from, to }
+}
+
 const init2D = (width) => Array(width).fill(null).map(() => [])
 
-const part1 = (path) => {
-    const applyRule = (rule) => {
-        const [, count, , from, , to] = rule.split(WORD);
-        for (let i = 0; i < count; i++) {
-            stacks[to].push(stacks[from].pop())
-        }
-    }
-
+const parseInput = (path) => {
     const [initialState, rules] =
-        fileToArr(path, EMPTY_LINE)
-            .map(x => x.split(LINE))
+    fileToArr(path, EMPTY_LINE)
+        .map(x => x.split(LINE))
 
     const stackCount = +countStacks(initialState)
 
     const stacks =
         initialState
             .reverse()
-            .reduce(buildStacks, init2D(stackCount+1))
+            .reduce(buildStacks, init2D(stackCount + 1))
 
+    const parsedRules = rules.map(parseRules)
+
+    return [stacks,parsedRules]
+}
+
+const part1 = (path) => {
+    const applyRule = ({ count, from, to }) => {
+        for (let i = 0; i < count; i++) {
+            stacks[to].push(stacks[from].pop())
+        }
+    }
+
+    const [stacks,rules] = parseInput(path)
     rules.forEach(applyRule)
     return stacks.map(stack => stack.pop()).join('')
 }
@@ -55,22 +63,8 @@ echo(`part1 -- example: ${part1(example)}`)
 echo(`part1 -- input: ${part1(input)}`)
 
 const part2 = (path) => {
-    const applyRule = (rule) => {
-        const [, count, , from, , to] = rule.split(WORD);
-        stacks[to].push(...stacks[from].splice(-count, count))
-    }
-
-    const [initialState, rules] =
-        fileToArr(path, EMPTY_LINE)
-            .map(x => x.split(LINE))
-
-    const stackCount = +countStacks(initialState)
-    
-    const stacks =
-        initialState
-            .reverse()
-            .reduce(buildStacks, init2D(stackCount + 1))
-   
+    const applyRule = ({ count, from, to }) => stacks[to].push(...stacks[from].splice(-count, count))
+    const [stacks,rules] = parseInput(path)
     rules.forEach(applyRule)
     return stacks.map(stack => stack.pop()).join('')
 }
--- 2022/js/day7/solution.mjs
+++ 2022/js/day7/solution.mjs
@@ -1,4 +1,67 @@
 #!/usr/bin/env zx
-const input="../../input/day7/input.txt"
-const example="../../input/day7/example.txt"
+import "zx/globals"
+import { fileToArr,sum, numSort } from "../helpers/index.mjs"
+const day = "day7";
+const input = `../../input/${day}/input.txt`
+const example = `../../input/${day}/example.txt`
 
+const TargetDirSize = 100000;
+const diskSize = 70000000;
+const requiredFree = 30000000;
+
+const parseInput = (path) => {
+    const dirs = [];
+    class Dir {
+        children = {};
+        size = 0;
+        constructor(name, parent) {
+            this.name = name;
+            this.parent = parent;
+        }
+        du = () => {
+            Object.values(this.children).forEach(child => this.size += child.du())
+            dirs.push(this.size);
+            return this.size;
+        }
+
+    }
+
+    const root = new Dir("/", null);
+    let pwd = root;
+
+    fileToArr(path).forEach(line => {
+        const args = line.split(' ');
+        switch(args[0]) {
+            case "$":
+                switch (args[1]) {
+                    case "cd":
+                        const target = args[2];
+                        pwd = (target === "/")
+                            ? root
+                            : (target === "..")
+                                ? pwd.parent
+                                : pwd.children[args[2]];
+                        break;
+                    case "ls":
+                    case "default":
+                }
+                break;
+            case "dir":
+                pwd.children[args[1]] = new Dir(args[0], pwd);
+                break;
+            default: //it's a folder
+                pwd.size += +args[0];
+        }
+    })
+    
+    const sizeToFree = requiredFree - (diskSize - root.du(root));
+    const smallestDeletable = dirs.filter(size => size >= sizeToFree).sort(numSort)[0];
+    const dirsUnderLimit = dirs.filter(size => size <= TargetDirSize).reduce(sum);
+    return [dirsUnderLimit, smallestDeletable];
+}
+
+echo(`part1 -- example: ${parseInput(example)[0]}`)
+echo(`part1 -- input: ${parseInput(input)[0]}`)
+
+echo(`part2 -- example: ${parseInput(example)[1]}`)
+echo(`part2 -- input: ${parseInput(input)[1]}`)
