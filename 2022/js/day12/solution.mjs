6b3833e549c755cf90536a07dd24d059dfa77c15
Douglas Hodson <douglas@r6digital.com.au>
2022-12-13 09:22:41 +1000

    cleanup solution for 2022/day12

--- 2022/js/day12/solution.mjs
+++ 2022/js/day12/solution.mjs
@@ -9,7 +9,7 @@ const fileToArr = (path, delim = '\n') => fs.readFileSync(path).toString().split
 
 class Node {
     dist = Number.MAX_SAFE_INTEGER;
-    constructor (x, y, z) {
+    constructor(x, y, z) {
         this.x = x;
         this.y = y;
         this.z = z;
@@ -17,88 +17,68 @@ class Node {
 }
 
 const part1 = (path) => {
-    let startNode;
+    let current;
     const nodes = fileToArr(path).map((row, y) =>
-            row.split('')
-                .map((char, x) => {
-                    if (char === 'S') {
-                        const z = 0;
-                        startNode = new Node(+x, +y, 1)
-                        return startNode;
-                    }
-                    if (char === 'E') {
-                        const z = 27;
-                        return new Node(+x, +y, +z)
-                    }
-                    const z = char.charCodeAt(0) - 96;
-                    return new Node(+x, +y, +z)
-                })
+        row.split('')
+            .map((char, x) => {
+                if (char === 'S') return current = new Node(x, y, 1)
+                if (char === 'E') return new Node(x, y, 27)
+                return new Node(x, y, char.charCodeAt(0) - 96)
+            })
     )
     const remaining = nodes.flat()
-    let current = startNode;
-    startNode.dist = 0;
+    current.dist = 0;
     while (true) {
-        if (current.z === 27) return current.dist
-        const newCost = current.dist + 1;
-        const neighbours = [
+        if (!current) break;
+        if (current.z === 27) return current.dist;
+        [
             nodes[current.y]?.[current.x - 1],
             nodes[current.y]?.[current.x + 1],
             nodes[current.y - 1]?.[current.x],
             nodes[current.y + 1]?.[current.x],
-        ]
-        neighbours.forEach(neighbour => {
+        ].forEach(neighbour => {
             if (neighbour?.z - 1 <= current.z
                 && remaining.includes(neighbour)
-                && neighbour?.dist > newCost
-            ) neighbour.dist = newCost;
+                && neighbour?.dist >= current.dist
+            ) neighbour.dist = current.dist+1;
         })
-        if (remaining.length === 0) break;
         current = remaining.sort((a, b) => a.dist - b.dist).shift();
-    }    
+    }
 }
 
-
 const part2 = (path) => {
-    let topNode;
+    let current;
     const nodes =
         fileToArr(path).map((row, y) =>
             row.split('')
                 .map((char, x) => {
-                    if (char === 'E') return topNode = new Node(x, y, 27)
+                    if (char === 'E') return current = new Node(x, y, 27)
                     if (char === 'S') return new Node(x, y, 1);
                     return new Node(x, y, char.charCodeAt(0) - 96)
                 }))
-
     const remaining = nodes.flat()
-    let current = topNode;
     current.dist = 0;
     while (true) {
-        const newCost = current.dist + 1;
-        const neighbours = [
+        if (!current) break;
+        [
             nodes[current.y]?.[current.x - 1],
             nodes[current.y]?.[current.x + 1],
             nodes[current.y - 1]?.[current.x],
             nodes[current.y + 1]?.[current.x],
-        ]
-        neighbours.forEach(neighbour => {
+        ].forEach(neighbour => {
             if (neighbour?.z + 1 >= current.z
                 && remaining.includes(neighbour)
-                && neighbour?.dist > newCost
-            ) neighbour.dist = newCost;
+                && neighbour?.dist >= current.dist
+            ) neighbour.dist = current.dist+1;
         })
-        if (remaining.length === 0) break;
         current = remaining.sort((a, b) => a.dist - b.dist).shift();
     }
-    return nodes
-        .flat()
-        .filter(n => n.z === 1)
-        .sort((a, b) => a.dist - b.dist)
-        .shift()
-        .dist;
+    return nodes.flat().filter(n => n.z === 1).sort((a, b) => a.dist - b.dist).shift().dist;
 }
-
+// const start = Date.now()
 console.log(`part1 -- example: ${part1(example)}`)
 console.log(`part1 -- input: ${part1(input)}`)
 
 console.log(`part2 -- example: ${part2(example)}`)
 console.log(`part2 -- input: ${part2(input)}`)
+// console.log(`took ${Date.now()-start}`)
