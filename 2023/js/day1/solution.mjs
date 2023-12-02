9609a59f537f883d10a057c4122f808f38b7984e
Douglas Hodson <douglas.j.hodson@gmail.com>
2023-12-02 15:36:21 +1000

    adding day2 initial solver

--- 2023/js/day1/solution.mjs
+++ 2023/js/day1/solution.mjs
@@ -7,7 +7,7 @@ const inputHandler = new InputHandler(process.cwd())
 const part1 = path => {
   const lines = inputHandler.toArray(path)
   return lines.reduce((sum, l) => {
-    const nums = l.replace(/[^0-9]/g, '').split('')
+    const nums = l.match(/\d/g)
     const [a] = nums
     const [b] = nums.slice(-1)
     return Number(`${a}${b}`) + Number(sum)
@@ -21,27 +21,60 @@ console.time('p1')
 console.log(`part1 -- input: ${JSON.stringify(part1(input))}`)
 console.timeEnd('p1')
 
+const numStrings = {
+  one: 1,
+  // eno: 1,
+  two: 2,
+  // owt: 2,
+  three: 3,
+  // eerht: 3,
+  four: 4,
+  // ruof: 4,
+  five: 5,
+  // evif: 5,
+  six: 6,
+  // xis: 6,
+  seven: 7,
+  // neves: 7,
+  eight: 8,
+  // thgie: 8,
+  nine: 9,
+  // enin: 9,
+}
+
+// const part2 = path => {
+//   const lines = inputHandler.toArray(path, line)
+//   return lines.reduce((sum, l) => {
+//     const [a] = l.match(/\d|one|two|three|four|five|six|seven|eight|nine/)
+//     const [b] = l
+//       .split('')
+//       .reverse()
+//       .join('')
+//       .match(/enin|thgie|neves|xis|evif|ruof|eerht|owt|eno|\d/g)
+//     const aa = isNaN(a) ? numStrings[a] : a
+//     const bb = isNaN(b) ? numStrings[b] : b
+//     console.log(`${l} ${aa}${bb}`)
+//     return Number(`${aa}${bb}`) + Number(sum)
+//   }, 0)
+// }
+
 const part2 = path => {
-  const numStrings = {
-    one: 'one1one',
-    two: 'two2two',
-    three: 'three3three',
-    four: 'four4four',
-    five: 'five5five',
-    six: 'six6six',
-    seven: 'seven7seven',
-    eight: 'eight8eight',
-    nine: 'nine9nine',
-  }
   const lines = inputHandler.toArray(path, line)
+  const regExp = /one|two|three|four|five|six|seven|eight|nine|\d/g
   return lines.reduce((sum, l) => {
-    Object.entries(numStrings).forEach(([k, v]) => {
-      l = l.replaceAll(k, v)
-    })
-    const nums = l.replace(/[^0-9]/g, '').split('')
-    const a = nums.at(0)
-    const b = nums.at(-1)
-    return Number(`${a}${b}`) + Number(sum)
+    const first = regExp.exec(l)
+    let last
+    let match
+    while ((match = regExp.exec(l))) {
+      // backtrack lastmatch pointer to catch overlapping words ie twone
+      if (match.index !== regExp.lastIndex - 1) regExp.lastIndex--
+      last = match[0]
+    }
+    const aa = isNaN(first) ? numStrings[first] : Number(first)
+    if (!last) return sum + aa * 11
+
+    const bb = isNaN(last) ? numStrings[last] : Number(last)
+    return sum + aa * 10 + bb
   }, 0)
 }
 
--- 2023/js/day2/solution.mjs
+++ 2023/js/day2/solution.mjs
@@ -1,42 +1,107 @@
 #!/usr/bin/env zx
-import 'zx/globals';
-import {
-    InputHandler,
-    example,
-    input,
-    line,
-    paragraph,
-    word,
-} from '../helpers/index.mjs';
+import 'zx/globals'
+import { InputHandler, example, input, line, paragraph, word } from '../helpers/index.mjs'
 
-const inputHandler = new InputHandler(process.cwd());
+const inputHandler = new InputHandler(process.cwd())
+
+const log = console.log
 
 const part1 = path => {
-    const chars = inputHandler.toArray(path, '');
-    const words = inputHandler.toArray(path, word);
-    const lines = inputHandler.toArray(path, line);
-    const paragraphs = inputHandler.toArray(path, paragraph);
-    return { chars, words, lines, paragraphs };
-};
+  const lines = inputHandler.toArray(path, line)
+  let sum = 0
+  lines.forEach(l => {
+    echo`${l}`
+    const [g, s] = l.split(':')
+    const game = g.split(' ')[1]
+    const rounds = s.split(';')
+    let valid = 1
+    rounds.forEach(r => {
+      // let red = +0
+      // let green = +0
+      // let blue = +0
 
-console.time('p1e');
-console.log(`part1 -- example: ${JSON.stringify(part1(example))}`);
-console.timeEnd('p1e');
-console.time('p1');
-console.log(`part1 -- input: ${JSON.stringify(part1(input))}`);
+      r.trim()
+        .split(',')
+        .map(x => x.trim().split(' '))
+        .forEach(p => {
+          // console.log(p)
+          if (p[1] === 'red') {
+            // red += +p[0]
+            if (p[0] > 12) {
+              // sum += +game
+              valid = 0
+              log(l)
+            }
+          } else if (p[1] === 'green') {
+            // green += +p[0]
+            if (p[0] > 13) {
+              // sum += +game
+              valid = 0
+              log(l)
+            }
+          } else {
+            // blue += +p[0]
+            if (p[0] > 14) {
+              // sum += +game
+              valid = 0
+              log(l)
+            }
+          }
+        })
+    })
+    log(l, valid)
+    if (valid) sum += +game
+
+    // log(rounds)
+    // echo`${game}`
+    // process.exit(0)
+  })
+  return sum
+}
+
+console.time('p1e')
+console.log(`part1 -- example: ${JSON.stringify(part1(example))}`)
+console.timeEnd('p1e')
+console.time('p1')
+console.log(`part1 -- input: ${JSON.stringify(part1(input))}`)
 console.timeEnd('p1')
 
 const part2 = path => {
-    const chars = inputHandler.toArray(path, '');
-    const words = inputHandler.toArray(path, word);
-    const lines = inputHandler.toArray(path, line);
-    const paragraphs = inputHandler.toArray(path, paragraph);
-    return { chars, words, lines, paragraphs }
-};
+  const lines = inputHandler.toArray(path, line)
+  let sum = 0
+  lines.forEach(l => {
+    echo`${l}`
+    const [g, s] = l.split(':')
+    const game = g.split(' ')[1]
+    const rounds = s.split(';')
+    let red = +0
+    let green = +0
+    let blue = +0
+    rounds.forEach(r => {
+      r.trim()
+        .split(',')
+        .map(x => x.trim().split(' '))
+        .forEach(p => {
+          // console.log(p)
+          if (p[1] === 'red') {
+            if (red < +p[0]) red = +p[0]
+          } else if (p[1] === 'green') {
+            if (green < +p[0]) green = +p[0]
+          } else {
+            if (blue < +p[0]) blue = +p[0]
+          }
+        })
+    })
+    log({ red, green, blue, power: red * green * blue })
+    // process.exit(0)
+    sum += red * green * blue
+  })
+  return sum
+}
 
-console.time('p2e');
-console.log(`part2 -- example: ${JSON.stringify(part2(example))}`);
-console.timeEnd('p2e');
-console.time('p2');
-console.log(`part2 -- input: ${JSON.stringify(part2(input))}`);
-console.timeEnd('p2');
+console.time('p2e')
+console.log(`part2 -- example: ${JSON.stringify(part2(example))}`)
+console.timeEnd('p2e')
+console.time('p2')
+console.log(`part2 -- input: ${JSON.stringify(part2(input))}`)
+console.timeEnd('p2')
