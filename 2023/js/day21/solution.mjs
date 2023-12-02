4adcada6866a789604f005e01746be688ddd7ac9
Douglas Hodson <douglas.j.hodson@gmail.com>
2023-12-02 15:36:50 +1000

    adding templates for day3-25

--- 2023/js/day21/solution.mjs
+++ 2023/js/day21/solution.mjs
@@ -1,4 +1,35 @@
 #!/usr/bin/env zx
-const input="../../input/day21/input.txt"
-const example="../../input/day21/example.txt"
+import 'zx/globals'
+import { InputHandler, example, input, line, paragraph, word } from '../helpers/index.mjs'
 
+const inputHandler = new InputHandler(process.cwd())
+
+const part1 = path => {
+  const chars = inputHandler.toArray(path, '')
+  const words = inputHandler.toArray(path, word)
+  const lines = inputHandler.toArray(path, line)
+  const paragraphs = inputHandler.toArray(path, paragraph)
+  return { chars, words, lines, paragraphs }
+}
+
+console.time('p1e')
+console.log(`part1 -- example: ${JSON.stringify(part1(example))}`)
+console.timeEnd('p1e')
+console.time('p1')
+console.log(`part1 -- input: ${JSON.stringify(part1(input))}`)
+console.timeEnd('p1')
+
+const part2 = path => {
+  const chars = inputHandler.toArray(path, '')
+  const words = inputHandler.toArray(path, word)
+  const lines = inputHandler.toArray(path, line)
+  const paragraphs = inputHandler.toArray(path, paragraph)
+  return { chars, words, lines, paragraphs }
+}
+
+console.time('p2e')
+console.log(`part2 -- example: ${JSON.stringify(part2(example))}`)
+console.timeEnd('p2e')
+console.time('p2')
+console.log(`part2 -- input: ${JSON.stringify(part2(input))}`)
+console.timeEnd('p2')
