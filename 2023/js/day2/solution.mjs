2055392801f7bb5f862bffca467823bf91afafcf
Douglas Hodson <douglas.j.hodson@gmail.com>
2023-12-02 17:27:48 +1000

    golfing 2023 day 2

--- 2023/js/day2/solution.mjs
+++ 2023/js/day2/solution.mjs
@@ -1,63 +1,70 @@
 #!/usr/bin/env zx
 import 'zx/globals'
-import { InputHandler, example, input, line, paragraph, word } from '../helpers/index.mjs'
+import {
+  InputHandler,
+  example,
+  input,
+  line as LINE,
+  product,
+  sum,
+  word,
+} from '../helpers/index.mjs'
 
 const inputHandler = new InputHandler(process.cwd())
 
-const log = console.log
-
-const part1 = path => {
-  const lines = inputHandler.toArray(path, line)
-  let sum = 0
-  lines.forEach(l => {
-    echo`${l}`
-    const [g, s] = l.split(':')
-    const game = g.split(' ')[1]
-    const rounds = s.split(';')
-    let valid = 1
-    rounds.forEach(r => {
-      // let red = +0
-      // let green = +0
-      // let blue = +0
-
-      r.trim()
-        .split(',')
-        .map(x => x.trim().split(' '))
-        .forEach(p => {
-          // console.log(p)
-          if (p[1] === 'red') {
-            // red += +p[0]
-            if (p[0] > 12) {
-              // sum += +game
-              valid = 0
-              log(l)
-            }
-          } else if (p[1] === 'green') {
-            // green += +p[0]
-            if (p[0] > 13) {
-              // sum += +game
-              valid = 0
-              log(l)
-            }
-          } else {
-            // blue += +p[0]
-            if (p[0] > 14) {
-              // sum += +game
-              valid = 0
-              log(l)
-            }
-          }
-        })
+const part1 = path =>
+  inputHandler
+    .toArray(path, LINE)
+    .map((line, gameId) => {
+      const picks = line
+        .split(':')[1]
+        .split(/;|,/)
+        .map(pick => pick.trim().split(word))
+      for (const [n, colour] of picks) {
+        switch (colour) {
+          case 'red':
+            if (n > 12) return 0
+            break
+          case 'green':
+            if (n > 13) return 0
+            break
+          case 'blue':
+            if (n > 14) return 0
+            break
+        }
+      }
+      return gameId
     })
-    log(l, valid)
-    if (valid) sum += +game
+    .reduce(sum)
 
-    // log(rounds)
-    // echo`${game}`
-    // process.exit(0)
-  })
-  return sum
-}
+const part2 = path =>
+  inputHandler
+    .toArray(path, LINE)
+    .map(line =>
+      line
+        .split(':')[1]
+        .split(/;|,/)
+        .reduce(
+          (acc, p) => {
+            const [n, c] = p.trim().split(' ')
+            switch (c) {
+              case 'red':
+                if (acc[0] <= +n) acc[0] = +n
+                break
+              case 'green':
+                if (acc[1] <= +n) acc[1] = +n
+                break
+              case 'blue':
+                if (acc[2] <= +n) acc[2] = +n
+                break
+            }
+            return acc
+          },
+          [0, 0, 0]
+        )
+        .reduce(product)
+    )
+    .reduce(sum)
 
 console.time('p1e')
 console.log(`part1 -- example: ${JSON.stringify(part1(example))}`)
@@ -65,39 +72,6 @@ console.timeEnd('p1e')
 console.time('p1')
 console.log(`part1 -- input: ${JSON.stringify(part1(input))}`)
 console.timeEnd('p1')
-
-const part2 = path => {
-  const lines = inputHandler.toArray(path, line)
-  let sum = 0
-  lines.forEach(l => {
-    echo`${l}`
-    const [g, s] = l.split(':')
-    const game = g.split(' ')[1]
-    const rounds = s.split(';')
-    let red = +0
-    let green = +0
-    let blue = +0
-    rounds.forEach(r => {
-      r.trim()
-        .split(',')
-        .map(x => x.trim().split(' '))
-        .forEach(p => {
-          // console.log(p)
-          if (p[1] === 'red') {
-            if (red < +p[0]) red = +p[0]
-          } else if (p[1] === 'green') {
-            if (green < +p[0]) green = +p[0]
-          } else {
-            if (blue < +p[0]) blue = +p[0]
-          }
-        })
-    })
-    log({ red, green, blue, power: red * green * blue })
-    // process.exit(0)
-    sum += red * green * blue
-  })
-  return sum
-}
 
 console.time('p2e')
 console.log(`part2 -- example: ${JSON.stringify(part2(example))}`)
--- 2023/js/helpers/index.mjs
+++ 2023/js/helpers/index.mjs
@@ -1,45 +1,38 @@
-export const example = 'example.txt';
-export const input = 'input.txt';
+export const example = 'example.txt'
+export const input = 'input.txt'
 
-export const word = ' ';
-export const line = '\n';
-export const paragraph = '\n\n';
+export const word = ' '
+export const line = '\n'
+export const paragraph = '\n\n'
 
 export class InputHandler {
   constructor(cwd) {
-    this.puzzleDay = path.basename(cwd);
-    this.inputDir = path.join(cwd, `../../input/${this.puzzleDay}`);
+    this.puzzleDay = path.basename(cwd)
+    this.inputDir = path.join(cwd, `../../input/${this.puzzleDay}`)
   }
-  toString = (file) => fs.readFileSync(`${this.inputDir}/${file}`).toString();
-  toArray = (file, delim = line) => this.toString(file).split(delim);
+  toString = file => fs.readFileSync(`${this.inputDir}/${file}`).toString()
+  toArray = (file, delim = line) => this.toString(file).split(delim)
 }
 
-export const fileToString = path => fs.readFileSync(path).toString();
-export const fileToArr = (path, delim = line) =>
-  fs.readFileSync(path).toString().split(delim);
-export const sum = (acc, cal) => Number(acc) + Number(cal);
-export const numSort = (a, b) => +a - b;
-export const numSortR = (a, b) => b - a;
+export const fileToString = path => fs.readFileSync(path).toString()
+export const fileToArr = (path, delim = line) => fs.readFileSync(path).toString().split(delim)
+export const sum = (acc, cal) => Number(acc) + Number(cal)
+export const product = (acc, cal) => acc * cal
+export const numSort = (a, b) => +a - b
+export const numSortR = (a, b) => b - a
 
-export const stringBisect = str => [
-  str.slice(0, str.length / 2),
-  str.slice(str.length / 2),
-];
-export const stringFindCommon = (a, b) => [...a].filter(a => b.includes(a));
+export const stringBisect = str => [str.slice(0, str.length / 2), str.slice(str.length / 2)]
+export const stringFindCommon = (a, b) => [...a].filter(a => b.includes(a))
 
-export const intDiv = (numerator, divisor) => [
-  Math.floor(numerator / divisor),
-  numerator % divisor,
-];
+export const intDiv = (numerator, divisor) => [Math.floor(numerator / divisor), numerator % divisor]
 export const reshape = (flat, width) =>
   flat.reduce((rect, cell, index) => {
-    const col = Math.floor(index / width);
-    const row = index % width;
-    rect[col] ??= [];
-    rect[col][row] = cell;
-    return rect;
-  }, []);
+    const col = Math.floor(index / width)
+    const row = index % width
+    rect[col] ??= []
+    rect[col][row] = cell
+    return rect
+  }, [])
 
-export const transpose = matrix => matrix.reduce((prev, next) =>
-  next.map((_, i) => (prev[i] ?? []).concat(next[i])), []
-);
+export const transpose = matrix =>
+  matrix.reduce((prev, next) => next.map((_, i) => (prev[i] ?? []).concat(next[i])), [])
